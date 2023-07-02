47547d0e218e6f79b7a833e6cab7d1e9aaea5152
Cerqueira <contatocerq@gmail.com>
2023-07-02 19:14:16 -0300

    feat: register controller

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,6 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
-prisma.user.create({
-  data: {
-    name: 'João Victor',
-    email: 'contatocerq@gmail.com',
-  },
-})
+import { appRoutes } from './http/routes'
 
 export const app = fastify()
+
+app.register(appRoutes)
