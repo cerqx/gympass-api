4fc0b6a2ea7e8a0db85a66ade830b78e56c77281
Cerqueira <contatocerq@gmail.com>
2023-07-01 23:35:58 -0300

    Initial prisma configs

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+prisma.user.create({
+  data: {
+    name: 'João Victor',
+    email: 'contatocerq@gmail.com',
+  },
+})
 
 export const app = fastify()
