cf3d96d92950341518b5c6d26040fefa7c55cd03
Cerqueira <contatocerq@gmail.com>
2023-07-07 14:33:06 -0300

    AuthenticateUseCase

---
+++ src/use-cases/authenticate.ts
@@ -0,0 +1,36 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { User } from '@prisma/client'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+import { compare } from 'bcryptjs'
+
+interface AuthenticateInterfaceRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticateInterfaceResponse {
+  user: User
+}
+
+export class AuthenticateUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateInterfaceRequest): Promise<AuthenticateInterfaceResponse> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doesPasswordsMatches = await compare(password, user.password_hash)
+
+    if (!doesPasswordsMatches) {
+      throw new InvalidCredentialsError()
+    }
+
+    return { user }
+  }
+}
