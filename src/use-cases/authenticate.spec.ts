636cbf97b052938c2c74c299717c7d475257f68e
Cerqueira <contatocerq@gmail.com>
2023-07-08 22:10:46 -0300

    Done

---
+++ src/use-cases/authenticate.spec.ts
@@ -0,0 +1,45 @@
+import { expect, describe, it } from 'vitest'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { AuthenticateUseCase } from './authenticate'
+import { hash } from 'bcryptjs'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+
+describe('Authenticate Use Case', () => {
+  it('should be able to authenticate', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const sut = new AuthenticateUseCase(usersRepository)
+
+    await usersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password_hash: await hash('123456', 6),
+    })
+
+    const { user } = await sut.execute({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should be not able to authenticate without email', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const sut = new AuthenticateUseCase(usersRepository)
+
+    await expect(() =>
+      sut.execute({
+        email: 'johndoe@example.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
+  })
+})
+
+// await expect(() =>
+// registerUseCase.execute({
+//   name: 'John Doe',
+//   email,
+//   password: '123456',
+// }),
+// ).rejects.toBeInstanceOf(UserAlreadyExistsError)
