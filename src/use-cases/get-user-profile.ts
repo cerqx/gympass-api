da79ba047c516d00aa2c74c848c5b56288d84549
Cerqueira <contatocerq@gmail.com>
2023-07-09 00:29:17 -0300

    Get User Profile Use Case and unit test

---
+++ src/use-cases/get-user-profile.ts
@@ -0,0 +1,26 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { User } from '@prisma/client'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+interface GetUserUseCaseRequest {
+  id: string
+}
+
+interface GetUserUseCaseResponse {
+  user: User
+}
+
+export class GetUserProfileUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    id,
+  }: GetUserUseCaseRequest): Promise<GetUserUseCaseResponse> {
+    const user = await this.usersRepository.findById(id)
+
+    if (!user) {
+      throw new ResourceNotFoundError()
+    }
+    return { user }
+  }
+}
