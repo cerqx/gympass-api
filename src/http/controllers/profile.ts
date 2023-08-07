165c8bbe603a574c4b72985492ea2ed12bfeeeed
Cerqueira <contatocerq@gmail.com>
2023-08-07 19:13:06 -0300

    Done

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,8 +1,17 @@
+import { makeGetUserProfileUseCase } from "@/use-cases/factories/make-get-user-profile-use-case";
 import { FastifyReply, FastifyRequest } from "fastify";
 
 export async function profile(request: FastifyRequest, response: FastifyReply) {
-  await request.jwtVerify();
-  console.log(request.user.sub);
+  const getUserProfile = makeGetUserProfileUseCase();
 
-  return response.status(201).send();
+  const { user } = await getUserProfile.execute({
+    id: request.user.sub,
+  });
+
+  return response.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  });
 }
